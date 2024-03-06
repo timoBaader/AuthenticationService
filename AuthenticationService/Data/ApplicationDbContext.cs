076e59c656258f8b61371ce3f1909f6662b224d6
Timo Baader <timo.baader91@gmail.com>
2024-03-06 09:11:11 +0200

    initial

---
+++ AuthenticationService/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using AuthenticationService.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthenticationService.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ AuthenticationService/Models/User.cs.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuthenticationService.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Username { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        [MaxLength(100)]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        public string PasswordHash { get; set; } = string.Empty;
+
+        public DateTime RegistrationDate { get; set; } = DateTime.UtcNow;
+    }
+}
